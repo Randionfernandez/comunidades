f47677d1da1b708a4581a25160a75cd64d0febfc
Francisco José Gordo Salguero <franciscojgsspam@gmail.com>
2021-06-21 18:25:46 +0200

    archivo olvidado del commit anterior

--- resources/views/comunidades/index.blade.php
+++ resources/views/comunidades/index.blade.php
@@ -7,13 +7,6 @@
         <hr>
     </x-slot>
 
-    <x-slot name="css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.8/css/fixedHeader.bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.bootstrap.min.css">
-    </x-slot>
-
     @if(! session()->get('activeCommunity'))
     @php $comunidades = $user->comunidades @endphp
     @else
@@ -66,35 +59,5 @@
     @else
     @include('partials.alert-notcreatedyet', ['emptyText1' => 'There are not communities created yet'])
     @endif
-
-    {{-- $comunidades->links() --}}
-
-    <x-slot name="js">
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-
-        <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
-        <script src="https://cdn.datatables.net/fixedheader/3.1.8/js/dataTables.fixedHeader.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.2.7/js/responsive.bootstrap.min.js"></script>
-        <script>
-                    $('#buscador').DataTable({
-                    resposive:true,
-                            autoWidth: false,
-                            "language": {
-                            "lengthMenu": "Mostrar _MENU_ registros por pagina",
-                                    "zeroRecords": "Nada encontrado  - disculpa",
-                                    "info": "Mostrando la pagina _PAGE_ de _PAGES_",
-                                    "infoEmpty": "No records available",
-                                    "infoFiltered": "(filtrado de _MAX_ registros totales)",
-                                    "search":'Buscar:',
-                                    "paginate": {
-                                    "next" : "Siguiente",
-                                            "previous": "Anterior"
-                                    }
-
-                            }
-                    });
-        </script>
-    </x-slot>
+    
 </x-app-layout>
