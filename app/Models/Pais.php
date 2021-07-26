52a0476a9cf7892336c71c1595d9ec0231dc09d3
Francisco José Gordo Salguero <franciscojgsspam@gmail.com>
2021-07-26 17:48:30 +0200

    unificando paises con la rama randion

--- app/Models/Pais.php
+++ app/Models/Pais.php
@@ -8,15 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pais extends Model {
 
     use HasFactory;
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> c4e5f520d6fa6d9bc9914a643a82cd2e2fe89654
     
->>>>>>> 480e0a25763c3e796ea0035d616d3a457377a7f7
     protected $table = 'paises';
     protected $keyType = 'string';
     protected $primaryKey= 'codigoISO';
